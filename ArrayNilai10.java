21a8c0da82a71a2d078ac2ece8aead33d085723b
gaduhprakoso <gaduhprakoso@gmail.com>
2024-10-31 09:23:09 +0700

    commit 2

---
+++ ArrayNilai10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayNilai10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukan Nilai Akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for(int i = 0; i<10;i++){
+            System.out.println("Nilai Akhir ke-"+i+" adalah "+ nilaiAkhir[i]);
+        }
+    }
+}
